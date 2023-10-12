98f1893b0e062ac47fce875f7cbd675ec5a63725
jackbelyeu <jackbelyeu@gmail.com>
2023-10-12 10:43:44 -0500

    added cors, helmet

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import cors from 'cors';
 import express from 'express';
+import helmet from 'helmet';
 import { PORT } from '@/config';
 
 export const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors());
+
+app.use(helmet());
+
+app.get('/user', (req, res) => {
+  res.send({ name: 'John Doe', email: 'johndoe@gmail.com', team: 'Yellow Mosquitos' });
 });
 
 if (import.meta.env.PROD) app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
