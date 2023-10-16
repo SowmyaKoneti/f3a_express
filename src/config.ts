a30fe78fa906477ec04ce17862cfcf3e3d8f6fc8
Tanzeel0651 <scientiest31@gmail.com>
2023-10-17 04:33:10 +0530

    Working reource

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const PORT = +import.meta.env.VITE_PORT || 3000;
+export const PORT = +import.meta.env.VITE_PORT || 3001;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import { PORT } from '@/config';
+import fs from 'fs';
 
 export const app = express();
 
@@ -12,5 +13,17 @@ app.use(helmet());
 app.get('/user', (req, res) => {
   res.send({ name: 'John Doe', email: 'johndoe@gmail.com', team: 'Yellow Mosquitos' });
 });
+app.get('/rules', (req, res) => {
+  var data = fs.readFileSync('/home/tanzeel/Videos/SLU/f3a-express/src/resource/rules.txt', 'utf8');
+  let arr = new Array();
+  data.split("/n").forEach(val => {
+    arr.push(val)
+  })
 
-if (import.meta.env.PROD) app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
+  res.json(arr);
+});
+
+// if (import.meta.env.PROD) app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`)
+});
