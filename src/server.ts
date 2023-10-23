a0c25fb95576077b7c0980ea287bb8cfe7c38d2f
jackbelyeu <jackbelyeu@gmail.com>
2023-10-23 16:54:24 -0500

    commented out db stuff so it works for others

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import express from 'express';
 import helmet from 'helmet';
 import type { QueryResult } from 'pg';
 import { PORT } from '@/config';
-import * as db from '@/db';
+// import * as db from '@/db';
 
 export const app = express();
 
@@ -14,19 +14,19 @@ app.use(helmet({ crossOriginResourcePolicy: false }));
 
 app.use(express.json());
 
-app.get('/games', async (req, res) => {
-  const result: QueryResult = await db.query('SELECT * FROM games');
-  res.send(result.rows[0]);
-});
+// app.get('/games', async (req, res) => {
+//   const result: QueryResult = await db.query('SELECT * FROM games');
+//   res.send(result.rows[0]);
+// });
 
-app.post('/games', async (req, res) => {
-  const { team1Name, team2Name, team1Score, team2Score } = req.body;
-  const query =
-    'INSERT INTO games (team_1_id, team_2_id, team_1_score, team_2_score) VALUES ($1, $2, $3, $4) RETURNING *';
-  const values = [team1Name, team2Name, team1Score, team2Score];
-  const result = await db.query(query, values);
-  res.send(result.rows[0]);
-});
+// app.post('/games', async (req, res) => {
+//   const { team1Name, team2Name, team1Score, team2Score } = req.body;
+//   const query =
+//     'INSERT INTO games (team_1_id, team_2_id, team_1_score, team_2_score) VALUES ($1, $2, $3, $4) RETURNING *';
+//   const values = [team1Name, team2Name, team1Score, team2Score];
+//   const result = await db.query(query, values);
+//   res.send(result.rows[0]);
+// });
 
 app.get('/rules', (req, res) => {
   const data = fs.readFileSync(`${process.cwd()}/src/resource/rules.txt`, 'utf8');
